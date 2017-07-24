147279e077b3db96d59d3bcc0dcffc2032441a80
baddah87 <alajmibaddah@gmail.com>
2017-07-24 19:38:13 +0300

    linking the list to the detail page

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.core.urlresolvers import reverse 
+
 class Post(models.Model):
 	title = models.CharField(max_length=50)
 	content = models.TextField()
@@ -11,4 +13,7 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title 
+
+	def get_absolute_url(self):
+		return reverse("posts:detail", kwargs={"post_id": self.id})
 # Create your models here.
