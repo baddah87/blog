7b4bfaa198ef1cf0a811ffa48f74e28866cbd288
baddah87 <alajmibaddah@gmail.com>
2017-07-25 18:47:11 +0300

    create, update, delete and some messages

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import PostForm
 from .models import Post
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
 # Create your views here.
 def post_list(request):
@@ -32,6 +33,36 @@ def post_detail(request, post_id):
 
 
 	return render(request, 'post_detail.html', context)
+
+def post_create(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "You created an object!!")
+		return redirect("posts:list")
+	context ={
+		"form" : form,
+	}
+	return render(request, 'post_create.html', context)
+
+
+def post_update(request, post_id):
+	post_object = get_object_or_404(Post, id=post_id)
+	form = PostForm(request.POST or None, instance=post_object)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "Are you sure?")
+		return redirect("posts:list")
+	context ={
+		"form" : form,
+		"post_object": post_object,
+	}
+	return render(request, 'post_update.html', context)
+def post_delete(request, post_id):
+	obj= Post.objects.get(id=post_id).delete()
+	messages.warning(request, "really???")
+	return redirect("posts:list")
+
 
 
 
