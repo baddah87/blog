87b715a12a39584978bda9fb6b1a137c7877348d
baddah87 <alajmibaddah@gmail.com>
2017-07-26 21:02:26 +0300

    adding images

--- posts/views.py
+++ posts/views.py
@@ -6,22 +6,31 @@ from .forms import PostForm
 from .models import Post
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
-	obj_list = Post.objects.all()#.order_by("-timestamp","-updated")
-	
-	context = {
-	
-	
-	"post_list" : obj_list,
-	
-	
+    obj_list = Post.objects.all()#.order_by("-timestamp","-updated")
 
-	}
-	return render(request, 'post_list.html', context)
+    paginator = Paginator(obj_list, 8) 
+    page = request.GET.get('page')
+    try:
+        objs = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        objs = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        objs = paginator.page(paginator.num_pages)
+    context = {
 
 
+    "post_list" : objs,
+
+
+
+    }
+    return render(request, 'post_list.html', context)
 
 
 
@@ -35,7 +44,7 @@ def post_detail(request, post_id):
 	return render(request, 'post_detail.html', context)
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		form.save()
 		messages.success(request, "You created an object!!")
@@ -48,7 +57,7 @@ def post_create(request):
 
 def post_update(request, post_id):
 	post_object = get_object_or_404(Post, id=post_id)
-	form = PostForm(request.POST or None, instance=post_object)
+	form = PostForm(request.POST or None,request.FILES or None, instance=post_object)
 	if form.is_valid():
 		form.save()
 		messages.success(request, "Are you sure?")
